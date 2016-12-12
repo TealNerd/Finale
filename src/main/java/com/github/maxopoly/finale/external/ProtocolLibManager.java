4f5b4c9fe41fbb9c80eac748fc7a45e887b508e1
biggestnerd <TealNerd@users.noreply.github.com>
2016-12-11 21:30:01 -0600

    Use dummy pearl instead of grabbing from a packet
    This is just a cleaner way to do it imo

--- src/main/java/com/github/maxopoly/finale/external/ProtocolLibManager.java
+++ src/main/java/com/github/maxopoly/finale/external/ProtocolLibManager.java
@@ -2,6 +2,7 @@ package com.github.maxopoly.finale.external;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import com.comphenix.protocol.PacketType;
@@ -14,14 +15,16 @@ import com.comphenix.protocol.events.PacketEvent;
 import com.github.maxopoly.finale.Finale;
 import com.github.maxopoly.finale.listeners.PearlCoolDownListener;
 
+import net.minecraft.server.v1_10_R1.ItemEnderPearl;
+
 public class ProtocolLibManager {
 	
 	private ProtocolManager protocolManager;
-	//no clue how to make this, so we snatch it from a normal packet
-	private Object pearlModifierObjectExample;
+	private ItemEnderPearl dummy;
 
 	public ProtocolLibManager() {
 		this.protocolManager = ProtocolLibrary.getProtocolManager();
+		dummy = new ItemEnderPearl();
 		registerPacketListener();
 	}
 
@@ -38,9 +41,6 @@ public class ProtocolLibManager {
 				PacketContainer packet = event.getPacket();
 				//ensure we are dealing with pearl cooldowns
 				if (packet.getModifier().read(0).getClass().getSimpleName().equals("ItemEnderPearl")) {
-					if (pearlModifierObjectExample == null) {
-						pearlModifierObjectExample = packet.getModifier().read(0);
-					}
 					if (PearlCoolDownListener.getPearlCoolDown(p.getUniqueId()) != packet.getIntegers().read(0)) {
 						//not our cooldown, so fuck it
 						event.setCancelled(true);
@@ -53,7 +53,7 @@ public class ProtocolLibManager {
 	public void sendPacketWithCoolDown(long coolDown, Player p) {
 		PacketContainer packet = new PacketContainer(PacketType.Play.Server.SET_COOLDOWN);
 		packet.getIntegers().write(0, (int) coolDown);
-		packet.getModifier().write(0, pearlModifierObjectExample);
+		packet.getModifier().write(0, dummy);
 		try {
 		    protocolManager.sendServerPacket(p, packet);
 		} catch (InvocationTargetException e) {
